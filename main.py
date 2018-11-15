ba2291b5496d937cad2b09c31b547001365e96d0
sheteng <sheteng@live.cn>
2018-11-15 17:39:46 +0800

    消息队列简版

--- main.py
+++ main.py
@@ -1,50 +1,49 @@
-import record
-import tts
+# import record
+# import tts
 import os
 import threading
 import queue
-
+import time
 FILNAME = "recordTem.mp3"
 FILE_SPEECH_IFK_TMP = "file_speech_ifk_tmp.wav"
-isTtsLoop = True
+isTtsLoop = True #对话循环
+isClose = False #主循环
+
 
 # socket 通信
 
 
+
+# Tts线程
+# def run():
+#     while isTtsLoop:
+#         # 录音
+#         record.recording(FILNAME)
+#         # 交互
+#         res = tts.ifkaiui(FILNAME)
+#         #
+#         # 合成
+#         tts.ifkspeech(res, FILE_SPEECH_IFK_TMP)
+#         # 播放
+#         record.playing(FILE_SPEECH_IFK_TMP)
+
 # 主循环
 q = queue.Queue()
 
-while not q.empty():
-    msg = q.get()
-
-
-
-# Tts线程
-def run():
-    while isTtsLoop:
-        # 录音
-        record.recording(FILNAME)
-        # 交互
-        res = tts.ifkaiui(FILNAME)
-        #
-        # 合成
-        tts.ifkspeech(res, FILE_SPEECH_IFK_TMP)
-        # 播放
-        record.playing(FILE_SPEECH_IFK_TMP)
-
-
-
 # 唤醒线程
 def wake():
-    print("wake")
-
-
+    print('thread %s ended.\n' % threading.current_thread().name)
+    time.sleep(5)
+    q.put("wake")
 #
-awake = threading.Thread(target=wake(), name='LoopThread')
+awake = threading.Thread(target=wake, name='LoopThread')
 awake.start()
 
-# 计时线程
-def timing(long):
-    pass
+print("loop start")
+while not isClose:
+    if not q.empty():
+        msg = q.get()
+        print(msg)
+        if msg == "wake":
 
-
+            pass
